05b80340629859f7ef78ac8885d83b8686120d72
Han <hannahannyshine@gmail.com>
2026-04-28 00:05:43 +0800

    Refactor database connection method in config.php

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
-$conn = new mysqli("localhost:3307", "root", "", "assignment_system");
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_connect("localhost:3307", "root", "", "assignment_system");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-
-session_start();
-?>
